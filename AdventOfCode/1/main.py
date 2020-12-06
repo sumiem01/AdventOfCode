f012bdb515039decf63f100f0c36fdbaf0cb170f
sumiem <msumiec@gmail.com>
2020-12-06 12:29:00 +0100

    Working count_to and count_three functions. Problem solved

---
+++ AdventOfCode/1/main.py
@@ -0,0 +1,40 @@
+PATH = "input1.txt"
+
+
+def get_data(path: str) -> list:
+    with open(path, "r") as f:
+        return f.readlines()
+
+
+def clean_input(data: list) -> list:
+    new_input: list = []
+    for i in data:
+        new_input.append(int(i.replace("\n", "")))
+    return new_input
+
+
+def count_two() -> list:
+    new_input = clean_input(get_data(PATH))
+    numbers: list = []
+    RANGE = len(new_input)
+    for i in range(RANGE):
+        for j in range(i, RANGE):
+            if new_input[i] + new_input[j] == 2020:
+                numbers.append(new_input[i] * new_input[j])
+    return numbers
+
+
+def count_three() -> list:
+    new_input = clean_input(get_data(PATH))
+    numbers: list = []
+    RANGE = len(new_input)
+    for i in range(RANGE):
+        for j in range(i, RANGE):
+            for k in range(j, RANGE):
+                if new_input[i] + new_input[j] + new_input[k] == 2020:
+                    numbers.append(new_input[i] * new_input[j] * new_input[k])
+    return numbers
+
+
+print(count_two())
+print(count_three())

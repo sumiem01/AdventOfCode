6db1598aa9f18f5872bd0f3d2f661b8c9f67feb5
sumiem <msumiec@gmail.com>
2020-12-08 15:48:49 +0100

    Day5 problem solved using simple counting of next elements of a list of seats_id

--- AdventOfCode/day5/main.py
+++ AdventOfCode/day5/main.py
@@ -9,17 +9,23 @@ def change_code_str_to_binary(code: str, one: str, zero: str) -> bin:
 
 
 def count_position(code: str) -> int:
+    """bin -> int"""
     return int(code, 2)
 
 
 def count_seat_id(code: str) -> int:
+    """seat_id = row_id * 8 + col_id
+    code: string, 10 letter code: first 7 letters are B or F representing row position Back or Forward,
+    last 3 letters are R or L representing column number Right or Left
+    """
     return (
             count_position(change_code_str_to_binary(code[:7], one="B", zero="F")) * 8 +
             count_position(change_code_str_to_binary(code[7:], one="R", zero="L"))
             )
 
 
-def check_maximum_seat_number(data: list):
+def check_maximum_seat_number(data: list) -> int:
+    """Return maximum seat_id in a given seat code list"""
     max_id: int = 0
     for code in data:
         if count_seat_id(code) > max_id:
@@ -27,8 +33,22 @@ def check_maximum_seat_number(data: list):
     return max_id
 
 
+def check_occupied_seats(data: list):
+    return [count_seat_id(code) for code in data]
+
+
+def find_your_seat(data: list) -> int:
+    for i, seat_id in enumerate(s[:-1]):
+        if s[i] + 1 != s[i + 1]:
+            return s[i] + 1
+    else:
+        return -1
+
+
 if __name__ == "__main__":
     path = r'C:\Users\Mateusz\Python\AdventOfCode\day5\day5_input.TXT'
     data = get_data(path)
     data = clean_new_line_char(data)
     print(check_maximum_seat_number(data))
+    s = sorted(check_occupied_seats(data))
+    print(find_your_seat(s))
